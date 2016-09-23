1bde462d3887b400172c084d7c51c312ca951639
Mon Pham <Mon Pham>
2016-09-23 09:51:44 +0700

    .pull from khoantk

--- Vienauto.Mobile/App_Start/BundleConfig.cs
+++ Vienauto.Mobile/App_Start/BundleConfig.cs
@@ -18,7 +18,7 @@ namespace VienautoRemake
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            //bundles.Add(new ScriptBundle("~/bundles/js/modernizr").Include(
+            //bundles.Add(new ScriptBundle("~/bundles/js/modernizr").Include( 
             //            "~/Assets/js/core/modernizr-*"));
 
             var coreJsVirtualPaths = BundleConfigHelper.GetVirtualPaths(BundConfigType.Js, "bunleCoreDirectory");
